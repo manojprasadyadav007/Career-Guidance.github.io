507db40b05257dc22b6557ae039a807cec96ec66
Manoj Yadav <manojfor4u@gmail.com>
2026-03-17 22:28:30 +0530

    refactor: remove unused image import from PlacedCandidate component

--- src/pages/PlacedCandidate.tsx
+++ src/pages/PlacedCandidate.tsx
@@ -5,7 +5,6 @@ import img1 from '../assets/img/img1.png'
 import img2 from '../assets/img/img2.png'
 import img3 from '../assets/img/img3.png'
 import img4 from '../assets/img/img4.png'
-import img5 from '../assets/img/img5.png'
 import img7 from '../assets/img/img7.png'
 import img6 from '../assets/img/img6.png'
 
